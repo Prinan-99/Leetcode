02e478bc19279e275a3016cd83f8a94781fda7f7
Prinan-99 <nandhini272005@gmail.com>
2023-07-05 11:11:59 +0530

    Rotating Image

---
+++ RotateImage.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int l=matrix.length;
+        int c[][]=new int[l][l];
+        for(int i=0;i<l;i++){
+            int trans=0;
+            int n=matrix[i].length;
+            for(int j=0;j<n;j++){
+                c[i][j]=matrix[n-j-1][i];
+            }
+        }
+        for(int i=0;i<l;i++){
+            for(int j=0;j<l;j++){
+               matrix[i][j]=c[i][j];
+            }
+        }
+    }
+}
